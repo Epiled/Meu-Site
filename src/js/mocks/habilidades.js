ccd918c82ed41a686e259d368a1700da0392d8ee
Felipe De Andrade <felipe.deam98@gmail.com>
2023-06-23 19:39:16 -0300

    Correção de path imagens de tecnologia usada

--- src/js/mocks/habilidades.js
+++ src/js/mocks/habilidades.js
@@ -5,59 +5,59 @@ const habilidades = {
   },
   css : {
     texto: 'CSS3',
-    imagem: './../../assets/svg/css3.svg',
+    imagem: './assets/svg/css3.svg',
   },
   javascript : {
     texto: 'Javascript',
-    imagem: './../../assets/svg/javascript.svg',
+    imagem: './assets/svg/javascript.svg',
   },
   php : {
     texto: 'PHP',
-    imagem: './../../assets/svg/php.svg',
+    imagem: './assets/svg/php.svg',
   },
   node : {
     texto: 'Node',
-    imagem: './../../assets/svg/node.svg',
+    imagem: './assets/svg/node.svg',
   },
   bootstrap : {
     texto: 'Bootstrap',
-    imagem: './../../assets/svg/bootstrap.svg',
+    imagem: './assets/svg/bootstrap.svg',
   },
   jquery : {
     texto: 'jQuery',
-    imagem: './../../assets/svg/jQuery.svg',
+    imagem: './assets/svg/jQuery.svg',
   },
   react : {
     texto: 'React',
-    imagem: './../../assets/svg/react.svg',
+    imagem: './assets/svg/react.svg',
   },
   sass : {
     texto: 'SASS',
-    imagem: './../../assets/svg/sass.svg',
+    imagem: './assets/svg/sass.svg',
   },
   gulp : {
     texto: 'Gulp',
-    imagem: './../../assets/svg/gulp.svg',
+    imagem: './assets/svg/gulp.svg',
   },
   figma : {
     texto: 'Figma',
-    imagem: './../../assets/svg/figma.svg',
+    imagem: './assets/svg/figma.svg',
   },
   handlebars : {
     texto: 'Handlebars',
-    imagem: './../../assets/svg/handlebars.svg',
+    imagem: './assets/svg/handlebars.svg',
   },
   mysql : {
     texto: 'MySQL',
-    imagem: './../../assets/svg/mysql.svg',
+    imagem: './assets/svg/mysql.svg',
   },
   pwa : {
     texto: 'PWA',
-    imagem: './../../assets/svg/pwa.svg',
+    imagem: './assets/svg/pwa.svg',
   },
   swiper : {
     texto: 'Swiper',
-    imagem: './../../assets/svg/swiper.svg',
+    imagem: './assets/svg/swiper.svg',
   }
 };
 
